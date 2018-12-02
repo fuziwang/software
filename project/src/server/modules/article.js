8de4cb61253c2dc3cf866867655373299b7bb83a
fuziwang <2622860598@qq.com>
2018-12-02 21:06:54 +0800

    server-update

--- project/src/server/modules/article.js
+++ project/src/server/modules/article.js
@@ -3,7 +3,7 @@ const db = require('./database.js');
 var Article = function(){};
 
 Article.prototype.getAll = function(cb){
-  const sql = 'select Article.aid,atitle,acontent,acomment,aimage,atime,GROUP_CONCAT(acolumn),aprivate,astatus from Article,ArticleColumn where Article.aid = ArticleColumn.aid group by Artcile.aid';
+  const sql = 'select Article.aid,atitle,acontent,acomment,aimage,atime,GROUP_CONCAT(acloumn),aprivate,astatus from Article,ArticleColumn where Article.aid = ArticleColumn.aid group by Article.aid';
   db.query(sql,(err,result)=>{
     if(err){
       cb(true);
@@ -12,6 +12,49 @@ Article.prototype.getAll = function(cb){
       cb(false,result);
     }
   })
+}
+
+Article.prototype.selectArticle = function(name,cb){
+  const sql = 'select Article.aid,atitle,acontent,acomment,aimage,atime,GROUP_CONCAT(acloumn),aprivate,astatus from Article,ArticleColumn where Article.aid = ArticleColumn.aid and atitle like ? group by Article.aid';
+  db.query(sql,['%' + name + '%'],(err,result)=>{
+    if(err){
+      cb(true);
+      return;
+    }
+    cb(false,result);
+  });
+}
+Article.prototype.updateItem = function(status,id,cb){
+  const sql = 'update Article set astatus = ? where aid = ?';
+  db.query(sql,[status,id],function(err,result){
+    if(err){
+      cb(true);
+      return;                
+    }
+    cb(false,result);      
+  });
+}
+
+Article.prototype.deleteItem = function(id,cb){
+    const sql = 'delete from Article where aid = ?';
+    db.query(sql,[id],function(err,result){
+      if(err){
+        cb(true);
+        return;
+      }
+      cb(false,result);
+    });
+}
+
+Article.prototype.deleteColumn = function(id,cb){
+    const sql = 'delete from ArticleColumn where aid = ?';
+    db.query(sql,[id],function(err,result){
+      if(err){
+        cb(true);
+        return;
+      }
+      cb(false,result);
+    });
 }
 
 module.exports = Article;
