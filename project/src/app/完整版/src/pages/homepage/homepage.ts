661715f9c5ad06b10e1c760ab152f510ab9123ea
duxue <1479605026@qq.com>
2018-12-09 14:43:01 +0800

    更新

--- project/src/app/完整版/src/pages/homepage/homepage.ts
+++ project/src/app/完整版/src/pages/homepage/homepage.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CreationPage } from '../creation/creation';
 import { FollowPage } from '../follow/follow';
 import { FansPage } from '../fans/fans';
+import { ApiProvider } from '../../providers/api/api';
 
 /**
  * Generated class for the HomepagePage page.
@@ -11,6 +12,21 @@ import { FansPage } from '../fans/fans';
  * Ionic pages and navigation.
  */
 
+interface user{
+  uid:number;
+  uname:string;
+  uimage:string;
+  usex:string;
+  uage:number;
+  uwhere:string;
+  utel:string;
+  upass:string;
+  ufans:number;
+  uconcern:number;
+  udescribe:string;
+  ustatus:number;
+}
+
 @IonicPage()
 @Component({
   selector: 'page-homepage',
@@ -18,7 +34,20 @@ import { FansPage } from '../fans/fans';
 })
 export class HomepagePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api:ApiProvider) {
+   
+    this.getList();
+    
+  }
+  list:Array<user>=[];
+  getList(){
+    //获取list用于显示
+    this.api.getMy().then(data=>{
+      //console.dir(data);
+      this.list=<any>data;
+      console.dir(this.list);
+    });
+    
   }
 
   creation(){
